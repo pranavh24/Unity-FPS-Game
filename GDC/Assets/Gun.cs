848ccb821a8a96ad06d709c18c27c8356e33e54e
Black3nsi <black3nsi@gmail.com>
2019-01-22 19:28:04 -0800

    FPS game made with Unity in C#

---
+++ GDC/Assets/Gun.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+public class Gun : MonoBehaviour {
+
+	public float damage = 10f;
+	public float range = 100f;
+
+	public Camera fpsCam;
+
+	void Update () {
+		if (Input.GetButtonDown("Fire1")) {
+			Shoot();
+		}
+	}
+
+	void Shoot() {
+		RaycastHit hit;
+		
+		if (Physics.Raycast(fpsCam.transform.position, fpsCam.transform.forward, out hit, range)) {
+			Debug.Log(hit.transform.name);
+		}	
+	}
+}
